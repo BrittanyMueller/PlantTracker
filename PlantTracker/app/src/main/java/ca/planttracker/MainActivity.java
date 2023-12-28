6638e48bb6670f30e096850430dbdabe204ee9ab
BrittanyMueller <brittany_mueller@sympatico.ca>
2023-12-28 16:14:18 -0500

    Initial test app

--- PlantTracker/app/src/main/java/ca/planttracker/MainActivity.java
+++ PlantTracker/app/src/main/java/ca/planttracker/MainActivity.java
@@ -2,11 +2,21 @@ package ca.planttracker;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+
+    public void clickMe(View view) {
+        EditText input = findViewById(R.id.inputField);
+        Toast.makeText(this, input.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
