277e1b9875fa6fb691f52f08a08a28be62a75a6f
BrittanyMueller <brittany_mueller@sympatico.ca>
2023-12-28 21:13:16 -0500

    Initial server setup, added server to pipeline

---
+++ server/app/src/test/java/planttracker/server/AppTest.java
@@ -0,0 +1,29 @@
+/*
+ * (C) Copyright 2023 Brittany Mueller and Larry Milne (https://www.larrycloud.ca)
+ *
+ * This code is distributed on "AS IS" BASIS,
+ * WITHOUT WARRANTINES OR CONDITIONS OF ANY KIND.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author: qawse3dr a.k.a Larry Milne
+ * @author: BrittanyMueller
+ */
+package planttracker.server;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test 
+    public void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+    @Test
+    public void fail() {
+        App classUnderTest = new App();
+        assertNotNull("app die", classUnderTest.getGreeting());
+    }
+}
