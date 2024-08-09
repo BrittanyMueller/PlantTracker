39a777f73047977cb0a1a50b7c6d618400dbbf34
Brittany Mueller <brittany_mueller@sympatico.ca>
2024-08-09 02:29:21 -0400

    Added image picker activity launcher.

--- android/plant_tracker/app/src/main/java/ca/planttracker/AddPlantActivity.java
+++ android/plant_tracker/app/src/main/java/ca/planttracker/AddPlantActivity.java
@@ -1,15 +1,51 @@
 package ca.planttracker;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class AddPlantActivity extends AppCompatActivity {
+
+    Button selectImageBtn;
+    ImageView plantImageView;
+
+    Uri imageUri;
+
+    private final ActivityResultLauncher<Intent> selectImageLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    imageUri = result.getData().getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                        plantImageView.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        Toast.makeText(AddPlantActivity.this, "Error selecting image.", Toast.LENGTH_SHORT).show();
+                        Log.e("PhotoPicker", Objects.requireNonNull(e.getMessage()));
+                    }
+                } else {
+                    // No image selected
+                    Toast.makeText(AddPlantActivity.this, "No image selected.", Toast.LENGTH_SHORT).show();
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +60,36 @@ public class AddPlantActivity extends AppCompatActivity {
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_arrow_24);
             getSupportActionBar().setTitle("");
         }
-
         TextView toolbarTitle = findViewById(R.id.toolbar_title);
         toolbarTitle.setText(getString(R.string.add_plant));
+
+        selectImageBtn = findViewById(R.id.upload_image_button);
+        plantImageView = findViewById(R.id.plant_image_view);
+
+        selectImageBtn.setOnClickListener(view -> selectImage());
+
+
+        TextInputEditText plantNameField = findViewById(R.id.plant_name_field);
 
         Button addPlantSubmit = findViewById(R.id.add_plant_submit);
         addPlantSubmit.setOnClickListener(view -> {
 
-            Client client = new Client("10.0.2.2", 5050);
-            long returnCode = client.addPlant();
+            String plantName = Objects.requireNonNull(plantNameField.getText()).toString();
+            Toast.makeText(AddPlantActivity.this, "Plant Name: " + plantName, Toast.LENGTH_LONG).show();
 
-            if (returnCode == 0) {
-                Toast.makeText(AddPlantActivity.this, "Add plant successful!", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(AddPlantActivity.this, "Add plant failed :(", Toast.LENGTH_SHORT).show();
-            }
+//            Client client = new Client("10.0.2.2", 5050);
+//            long returnCode = client.addPlant();
+//
+//            if (returnCode == 0) {
+//                Toast.makeText(AddPlantActivity.this, "Add plant successful!", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(AddPlantActivity.this, "Add plant failed :(", Toast.LENGTH_SHORT).show();
+//            }
         });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        selectImageLauncher.launch(intent);
     }
 }
